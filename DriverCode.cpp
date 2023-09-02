1210a4cd6552543113ba78156f549d119838b085
Szymon Rogowski <szymon0r0official@gmail.com>
2023-09-02 22:59:49 +0200

    refactoring from 2nd and 3rd term at uni :)

--- DriverCode.cpp
+++ DriverCode.cpp
@@ -1,14 +1,17 @@
 #include "BuiltInLibs.h"
-#include "Stack.h"
-#include "Queue.h"
-#include "LinkedList.h"
-#include "Sortings.h"
-#include "Dijkstra.h"
-#include "Knuth-Morris-Pratt.h"
-#include "Kruskal.h"
-#include "TreesAVL.h"
-#include "TreesRB.h"
-#include "Prim.h"
+#include "basic/Stack.h"
+#include "basic/Queue.h"
+#include "basic/LinkedList.h"
+#include "basic/Sortings.h"
+#include "graphs/Dijkstra.h"
+#include "graphs/Prim.h"
+#include "graphs/Kruskal.h"
+#include "graphs/BellmanFord.h"
+#include "string-search/Boyer-Moor.h"
+#include "string-search/Knuth-Morris-Pratt.h"
+#include "string-search/Rabin_Karp.h"
+#include "trees/TreesAVL.h"
+#include "trees/TreesRB.h"
 
 int main(){
     cout << "WorkingDriverCode\n";
@@ -16,12 +19,11 @@ int main(){
     Stack ObjectStack;
     Queue ObjectQueue;
     LinkedList ObjectLinkedList;
-
-    node ObjectSort;
+    Sortings ObjectSort{};
 
 //    ObjectStack.driverCodeStack();
 //    ObjectQueue.driverCodeQueue();
 //    ObjectLinkedList.driverCodeLinkedList();
-    ObjectSort.driverCodeSort();
+//    ObjectSort.driverCodeSort();
     return 0;
 }
