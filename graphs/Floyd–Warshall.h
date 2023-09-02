f2857f5b05dc8cb751360c9be263b0b5086352d7
Szymon Rogowski <szymon0r0official@gmail.com>
2023-09-02 23:20:37 +0200

    refactoring from 2nd and 3rd term at uni :)

---
+++ graphs/Floyd–Warshall.h
@@ -0,0 +1,10 @@
+//
+// Created by szymo on 02.09.2023.
+//
+
+#ifndef ALGOSANDDATASTRUCTURES_FLOYD_WARSHALL_H
+#define ALGOSANDDATASTRUCTURES_FLOYD_WARSHALL_H
+// https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm
+// https://www.geeksforgeeks.org/floyd-warshall-algorithm-dp-16/
+// https://www.programiz.com/dsa/floyd-warshall-algorithm
+#endif //ALGOSANDDATASTRUCTURES_FLOYD_WARSHALL_H
