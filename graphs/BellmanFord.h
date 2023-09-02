396bcdb6b648c73adef1a43d67b26e070b6255e5
Szymon Rogowski <szymon0r0official@gmail.com>
2023-09-02 23:00:15 +0200

    refactoring from 2nd and 3rd term at uni :)

---
+++ graphs/BellmanFord.h
@@ -0,0 +1,10 @@
+//
+// Created by szymo on 08.03.2023.
+//
+
+#ifndef ALGOSANDDATASTRUCTURES_BELLMANFORD_H
+#define ALGOSANDDATASTRUCTURES_BELLMANFORD_H
+// #####################################################################################################################
+// https://www.geeksforgeeks.org/bellman-ford-algorithm-simple-implementation/
+// #####################################################################################################################
+#endif //ALGOSANDDATASTRUCTURES_BELLMANFORD_H
---
+++ trees/TreesAVL.h
@@ -0,0 +1,13 @@
+//
+// Created by szymo on 08.03.2023.
+//
+
+#ifndef ALGOSANDDATASTRUCTURES_TREESAVL_H
+#define ALGOSANDDATASTRUCTURES_TREESAVL_H
+// #####################################################################################################################
+// https://en.wikipedia.org/wiki/AVL_tree
+// not in cpp
+// disk implementation
+// complete in the future (possibly)
+// #####################################################################################################################
+#endif //ALGOSANDDATASTRUCTURES_TREESAVL_H
